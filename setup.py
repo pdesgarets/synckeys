21b86d009d61e50bf965083acd5bf802f11b357a
Paul des Garets <paul.des-garets@illuin.tech>
2022-03-28 19:05:05 +0200

    Upgrade Ansible and change logging level for success

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ansible>=2.4,<2.8'],
+    install_requires=['ansible>=5.0.0,<6.0.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
